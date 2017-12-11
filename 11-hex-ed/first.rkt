b0177d7e4ddf5ecd316a46cc9070b4c3a3a3ee0c
Michal Hojgr <michal.hojgr@gmail.com>
2017-12-11 09:11:33 +0100

    add day 11 solution

---
+++ 11-hex-ed/first.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+(define (direction->vector dir)
+  (match dir
+    ["nw" (vector -1/2 1/2)]
+    ["n" (vector 0 1)]
+    ["ne" (vector 1/2 1/2)]
+    ["se" (vector 1/2 -1/2)]
+    ["s" (vector 0 -1)]
+    ["sw" (vector -1/2 -1/2)]))
+
+(define (advance pos change-vec)
+  (match-let* ([(vector x y) pos]
+         [(vector plus-x plus-y) change-vec]
+         [new-x (+ x plus-x)]
+         [new-y (+ y plus-y)])
+    (vector new-x new-y)))
+
+(define (solve moves [pos (vector 0 0)])
+  (match moves
+    [(cons move rest-moves) (solve rest-moves (advance pos (direction->vector move)))]
+    [_ (hex-distance pos)]))
+
+(define (hex-distance pos)
+  (match-let* ([(vector x y) pos]
+               [x-steps (abs x)]
+               [y-steps (abs y)])
+    (+ x-steps y-steps)))
+
+(solve (string-split (read-line) ","))
